deb5ee62551e83c01c07f9ec1b11de10819ace4f
Jack Matier <self@jackalyst.com>
2018-02-25 15:37:32 -0700

    Documentation Navigation Improvements

--- assets/main.js
+++ assets/main.js
@@ -23,6 +23,21 @@ $(document).ready(function() {
         $('.slideshow img').css('max-height',minsize+"px");
     });
     
+var sections = $('.document-page h2, .document-page h3'), nav = $('.floatmenu nav ul'), nav_height = nav.outerHeight();
+ 
+$(window).on('scroll load', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function(k, item) {
+    if($(this).offset().top > cur_pos - 10) {
+        var link = $(this).find('a').attr('href');
+        nav.find('li a').parent().removeClass('active')
+        nav.find('li a[href="'+link+'"]').parent().addClass('active')
+        return false;
+    }
+  });
+});
+
     $(function() {
       // This is a bit silly - but medium has CORS.
       var $content = $(".blogroll");
