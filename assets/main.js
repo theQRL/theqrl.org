5d30922c2abf89fc3ee84493e00b804064097129
Jack Matier <self@jackalyst.com>
2018-02-25 02:39:31 -0700

    Faster + Firefox fix

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
     // Run through slideshow
     $(window).on('load resize', function() {
-        
+    
         $('.slideshow img').css('max-height','none');
 
         var sizes = [];
@@ -18,8 +18,8 @@ $(document).ready(function() {
         });
         var minsize = Math.min.apply(null, sizes);
         $('.slideshow img').css('max-height',minsize+"px");
-
     });
+    
     $(function() {
       // This is a bit silly - but medium has CORS.
       var $content = $(".blogroll");
@@ -81,3 +81,33 @@ $(document).ready(function() {
     });
     
   });
+    /* Light YouTube Embeds by @labnol */
+    /* Web: http://labnol.org/?p=27941 */
+
+    document.addEventListener("DOMContentLoaded",
+        function() {
+            var div, n,
+                v = document.getElementsByClassName("youtube-player");
+            for (n = 0; n < v.length; n++) {
+                div = document.createElement("div");
+                div.setAttribute("data-id", v[n].dataset.id);
+                div.innerHTML = labnolThumb(v[n].dataset.id);
+                div.onclick = labnolIframe;
+                v[n].appendChild(div);
+            }
+        });
+
+    function labnolThumb(id) {
+        var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+            play = '<div class="play"></div>';
+        return thumb.replace("ID", id) + play;
+    }
+
+    function labnolIframe() {
+        var iframe = document.createElement("iframe");
+        var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
