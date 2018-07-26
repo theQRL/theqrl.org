2ac223303aaa6fa2bda5a8d9731aa075ebad4c15
Jack Matier <self@jackalyst.com>
2018-07-26 09:47:33 -0600

    In The Media Page
    + API documentation link in footer
    + Pulling in the blog section locally.

--- _scripts/test.build.sh
+++ _scripts/test.build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+PORT=4000
+
+re='^[0-9]+$'
+if [[ -n "$1" ]]; then
+	if ! [[ "$1" =~ $re ]] ; then
+	   echo "Port needs to be a number" >&2; exit 1
+	fi
+
+	PORT="$1"
+fi
+
+curl -o assets/medium.rss "https://medium.com/feed/the-quantum-resistant-ledger/"
 
 # Because `bundle exec jekyll serve` isn't the same as `bundle exec jekyll build` - apparently.
-bundle exec jekyll build --watch & php -S localhost:4000 -t _site/ && fg
+bundle exec jekyll build --watch & php -S localhost:"$PORT" -t _site/ && fg
