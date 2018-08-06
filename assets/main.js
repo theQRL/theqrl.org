368f6da33e2669df8e244d9abcbf958c9f650f66
Jack Matier <self@jackalyst.com>
2018-08-05 19:33:34 -0600

    Add GA

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-123414102-1');
 
 $(document).ready(function() {
 
