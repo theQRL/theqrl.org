b976866da7e953067ea0d8e9ed00f1d58df93609
JP Lomas <jp@theqrl.org>
2020-11-16 10:32:20 +0000

    Doublequotes for Codacy

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,21 +1,21 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './public',
+      staticDistDir: "./public",
       url: [
-            '/index.html',
-            '/404.html',
+            "/index.html",
+            "/404.html",
            ],
       numberOfRuns: 5
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', {minScore: 0.7}],
-        'categories:accessibility': ['error', {minScore: 0.5}],
+        "categories:performance": ["error", {minScore: 0.7}],
+        "categories:accessibility": ["error", {minScore: 0.5}],
       },
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: "temporary-public-storage",
     },
   },
 };
