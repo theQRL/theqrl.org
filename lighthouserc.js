0ea382ecabf46edb82b0ba60e802249eab9f3365
JP Lomas <jp@theqrl.org>
2020-11-16 10:19:22 +0000

    Add Lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './public',
+      url: [
+            '/index.html',
+            '/404.html',
+           ],
+      numberOfRuns: 5
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', {minScore: 0.7}],
+        'categories:accessibility': ['error', {minScore: 0.5}],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
