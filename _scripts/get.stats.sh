eddce32709668d272006620e4e0a04879d37242c
Jack Matier <self@jackalyst.com>
2022-01-26 11:12:24 -0700

    Add script to update various statistics

---
+++ _scripts/get.stats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -f "config.toml" ]; then
+	echo "ERROR: Make sure you call this from the root hugo directory"
+	exit 1;
+fi
+
+wget -O data/emission.json https://explorer.theqrl.org/api/emission
+wget -O data/reward.json https://explorer.theqrl.org/api/reward
+wget -O static/_data/networktx.json https://analytics.quantascan.io/api/network-transactions
+wget -O static/_data/numberwallets.json https://analytics.quantascan.io/api/network-transactions
+wget -O data/miningstats.json https://explorer.theqrl.org/api/miningstats
