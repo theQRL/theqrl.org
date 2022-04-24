515fe52867af55530771b13d91dd4ae54c3cb115
Jack Matier <self@jackalyst.com>
2022-04-24 13:38:45 -0600

    Add GA4 Header

---
+++ assets/js/header.js
@@ -0,0 +1,7 @@
+if ( navigator.doNotTrack != "yes" && navigator.doNotTrack != "1" && window.doNotTrack != "1" && navigator.msDoNotTrack != "1" ){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-3GWG6HEE2T');
+}
