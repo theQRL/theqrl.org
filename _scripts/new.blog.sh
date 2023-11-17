8938cbf6fb2bbf7ca29c0e7cfcd9c068eb99e9cb
Jack Matier <self@jackalyst.com>
2023-11-16 17:10:23 -0700

    Improve blog bootstrap file

--- _scripts/new.blog.sh
+++ _scripts/new.blog.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Script uses environment variables
+#
+# Set HACKMD_BEARER_TOKEN in ~/.bashrc
+# Set REMOTE_IP in ~/.bashrc as well
 echo "Current directory: $(pwd)"
 
 # Check whether we're calling this from the root directory
@@ -8,7 +12,7 @@ if [ ! -f "config.toml" ]; then
 	exit 1;
 fi
 
-blogtocopy="last-month-at-qrl-august-2022"
+blogtocopy="qrl-monthly-feat-the-history-of-cryptography-may-2023"
 name=$1
 
 if [[ -z ${1} ]]; then
@@ -16,7 +20,7 @@ if [[ -z ${1} ]]; then
 	exit 1;
 fi
 
-slug=$(echo $name | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:] ._' | tr -s ' ' | tr ' ' '-')
+slug=$(echo $name | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:] _' | tr -s ' ' | tr ' ' '-')
 
 # File locations
 assets_dir=assets/content/blog/"$(date +%Y)"/"$slug"/
@@ -32,7 +36,7 @@ _author="Jack Matier"
 _summary=""
 
 echo "... copying assets from: $blogtocopy -> $name"
-cp -r assets/content/blog/2022/"$blogtocopy"/. "$assets_dir"
+cp -r assets/content/blog/2023/"$blogtocopy"/. "$assets_dir"
 sleep 1
 
 echo "... removing images except for header.png"
@@ -47,6 +51,43 @@ echo "... writing to file:"
 echo "- date: $_date"
 echo "- title: $_title"
 echo "- slug: $_slug"
+
+
+content=$(cat << EOF
+---
+layout: $_layout
+slug: $_slug
+date: $_date
+author: "$_author"
+title: "$_title"
+summary: "$_summary"
+featured_image: header.png
+categories:
+  - technical
+features:
+  - nfts
+toc: true
+---
+
+###### tags: \`blog\` \`staging\`
+
+Testing $_title
+EOF
+)
+
+contentdata=$(echo -n "$content" | sed -e '$ ! s/$/\\n/' | sed 's/"/\\"/g' | tr -d \\n)
+
+gencontent() 
+{
+cat <<EOF
+{
+	"content": "$contentdata",
+	"commentPermission": "everyone",
+	"readPermission": "guest",
+	"writePermission": "guest"
+}
+EOF
+}
 
 cat << EOF >| ${post_file}
 ---
@@ -64,3 +105,20 @@ features:
 toc: true
 ---
 EOF
+
+
+
+
+postdata=$(curl -s -H 'Authorization: Bearer '$HACKMD_BEARER_TOKEN'' -H 'Content-Type: application/json' --location 'https://api.hackmd.io/v1/notes' -d "$(gencontent)")
+
+resultid=$(echo "$postdata" | jq -r .id)
+
+echo "$(cat <<EOF
+Title: $_title
+Blog: http://$REMOTE_IP:1313/blog/$_slug
+Edit: https://hackmd.io/$resultid
+
+# Add to blog.csv
+echo "$resultid,$_slug,TRUE" >> blog.csv
+EOF
+)"
